68183f7215d3eb6497009b7f68a708d3ed540bd9
saif1790 <saif.tech1790@gmail.comm>
2023-11-15 08:46:42 +0530

    updated a method with new approach

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+        public static String longestCommonPrefix(String[] s) {
+            Arrays.sort(s);//["flight","flow","flower"]
+            StringBuilder sb = new StringBuilder();
+            char[] firstWord = s[0].toCharArray();//[f,l,i,g,h,t]
+            char[] lastWord =  s[s.length-1].toCharArray();//[f,l,o,w,e,r]\
+
+            //comparing the character from both i.e firstWord and lastWord
+            for(int i=0;i<firstWord.length;i++)
+            {
+                if(firstWord[i] != lastWord[i])
+                    break;
+                sb.append(firstWord[i]);
+            }
+            return  sb.toString();
+        }
+        public static void main(String[] args) {
+            // Your code goes her
+            String[] s = {"flower", "flow", "floght"};
+
+            System.out.println("Longest common prefix in given array :"+longestCommonPrefix(s));
+        }
+}
