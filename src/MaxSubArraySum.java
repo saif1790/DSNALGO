2a1ffdd3fdf2cf48eb23d6c45e255322b66d4292
saif1790 <saif.tech1790@gmail.comm>
2023-11-25 16:33:45 +0530

    updated a method with new approach

---
+++ src/MaxSubArraySum.java
@@ -0,0 +1,33 @@
+//Kadanes Algorithm
+
+public class MaxSubArraySum {
+    public static void main(String[] args) {
+
+        int[] a = {-2, -5, 6, -2, -3, 1, 5, -6};
+
+        int max_sum = maxSubArraySum(a);
+
+        System.out.println("max_sum :" + max_sum);
+
+    }
+
+    public static int maxSubArraySum(int[] a) {
+
+        //To find the possible maximum sum
+        int max_sum = a[0];//-2
+
+        //To store the maximum found at a position
+        int cur_max = a[0];//-2
+
+        for(int i =1;i<a.length; i++)
+        {
+
+            cur_max = Math.max(a[i],cur_max + a[i]);
+
+            max_sum = Math.max(cur_max,max_sum);
+        }
+
+
+        return max_sum;
+    }
+}
