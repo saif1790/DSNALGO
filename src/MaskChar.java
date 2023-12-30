d73e1645fd4da4584d63c0246035c7409549abfc
saif1790 <saif.tech1790@gmail.comm>
2023-12-30 13:20:03 +0530

    updated...

--- src/MaskChar.java
+++ src/MaskChar.java
@@ -3,20 +3,24 @@ import java.util.Base64;
 public class MaskChar {
 
     public static void main(String[] args) {
-        String carNumber = "786543889986";
-        String s1 = maskChar(carNumber);
-        System.out.println("Credit Card Number "+carNumber+" after masking : "+s1);
+        String carNumber = "987612340008";
+        String s1 = maskChar(carNumber, 'X');
+        System.out.println("Credit Card Number " + carNumber + " after masking : " + s1);
     }
 
-    public static String maskChar(String s) {
+    public static String maskChar(String cardNumber, char maskChar) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 4) {
-                sb.append("X");
-            } else {
-                sb.append(s.charAt(i));
+        int creditCardLength = cardNumber.length();
+        if (cardNumber != null && cardNumber.length() > 4) {
+            for (int i = 0; i < creditCardLength; i++) {
+                if(i <  (creditCardLength - 4)) {
+                    sb.append(maskChar);
+                }else{
+                    sb.append(cardNumber.charAt(i));
+                }
             }
+            return sb.toString();
         }
-        return sb.toString();
+        return sb.append(cardNumber).toString();
     }
 }
