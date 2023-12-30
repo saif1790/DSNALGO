2e2d3e95d57b0501bf7595eaa86691ce6dc7e910
saif1790 <saif.tech1790@gmail.comm>
2023-12-30 12:06:52 +0530

    Masking Character except last 4 character

---
+++ src/MaskChar.java
@@ -0,0 +1,22 @@
+import java.util.Base64;
+
+public class MaskChar {
+
+    public static void main(String[] args) {
+        String carNumber = "7290859582";
+        String s1 = maskChar(carNumber);
+        System.out.println("Credit Card Number "+carNumber+" after masking : "+s1);
+    }
+
+    public static String maskChar(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 4) {
+                sb.append("X");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
