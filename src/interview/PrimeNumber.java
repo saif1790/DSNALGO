0374af920d38ff46c4156e5e41c88931c62f358e
saif1790 <saif.tech1790@gmail.comm>
2024-05-02 09:59:14 +0530

    Find prime number from 1-any numner

---
+++ src/interview/PrimeNumber.java
@@ -0,0 +1,29 @@
+package interview;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+        for(int i= 1; i<=n; i++) {
+            boolean isPrimeNumber = isPrime(i);
+            if(isPrimeNumber) {
+                System.out.println(i + "Number is prime");
+            }
+        }
+            }
+
+    public static boolean isPrime(int n) {
+
+        if (n < 2)
+            return false;
+
+        /* taken n/2 because to reduce the number of iteration,if any number is divisible by
+         2 and having more than 2 factor that number is not prime */
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
